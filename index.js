9e6504051ae946e2d66f3bc601a4484e0c4ad7be
apoorwa1 <apoorwa.saxena@gmail.com>
2021-12-06 20:30:00 +0000

    part2 done

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ let myCollection = [
     }
   ];
 
-  console.log(myCollection);
-  console.log(myCollection[0].name);
+  //console.log(myCollection);
+  //console.log(myCollection[0].name);
     /* Create a function called describeItem, which should take in an item as a parameter (the argument handed to this function would be an item from our collection). The function should console.log a message according to how many of the item you have.
 
 If there's only one of the item in your collection, it should log "I have a name. Here's what I like about it: whatILike". If you have more than one of it, the message should log "I have count names. Here's what I like about them: whatILike".
@@ -31,15 +31,29 @@ Now run the file again (using node index.js);*/
 function describeItem(item){
  //push(itemArr[item]);
  //console.log(itemArr.length);
- for(let i=0; i< myCollection.length; i++){
-
+ //for(let i=0; i< myCollection.length; i++){
+//let i;
  
- if (item[i].count === 1){
-     console.log(`I have a ${item[i].name}. Here's what I like about it: ${item[i].whatILike}`);
+ if (item.count === 1){
+     console.log(`I have a ${item.name}. Here's what I like about it: ${item.whatILike}`);
       }
       else {
-          console.log(`I have ${item[i].count} ${item[i].name}. Here's what I like about them: ${item[i].whatILike}`);
+          console.log(`I have ${item.count} ${item.name}. Here's what I like about them: ${item.whatILike}`);
       }
 }
+//}
+//describeItem(myCollection[0]);
+
+/*Task 2d*/
+
+//creating function describeCollection which takes an array as parameter
+let Arr = [];
+function describeCollection(Arr){
+//for(let item = 0; item < Arr.length; item++){
+
+  Arr = Arr.forEach(describeItem);
+
 }
-describeItem(myCollection);
+describeCollection(myCollection);
+
+
