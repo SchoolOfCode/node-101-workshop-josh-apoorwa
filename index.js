09ede03ac22fc2a718158cbedeab586ca29555a7
apoorwa1 <apoorwa.saxena@gmail.com>
2021-12-06 23:15:14 +0000

    Part 4 done

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 import myCollection from "./collection.js";
+import chalk from "chalk";
 
   //console.log(myCollection);
   //console.log(myCollection[0].name);
-    /* Create a function called describeItem, which should take in an item as a parameter (the argument handed to this function would be an item from our collection). The function should console.log a message according to how many of the item you have.
+   
 
-If there's only one of the item in your collection, it should log "I have a name. Here's what I like about it: whatILike". If you have more than one of it, the message should log "I have count names. Here's what I like about them: whatILike".
-
-For example, the result of calling the describeItem function with the first item in our collection would be: "I have a School of Code mug. Here's what I like about it: It has my cute pixel character on it!"
-
-Call your function below where you've defined it, handing in the first item in myCollection.
-
-Now run the file again (using node index.js);*/
 //let item;
 function describeItem(item){
  //push(itemArr[item]);
@@ -19,10 +13,10 @@ function describeItem(item){
 //let i;
  
  if (item.count === 1){
-     console.log(`I have a ${item.name}. Here's what I like about it: ${item.whatILike}`);
+     console.log(`I have a ${chalk.cyan(item.name)}. Here's what I like about it: ${chalk.green(item.whatILike)}`);
       }
       else {
-          console.log(`I have ${item.count} ${item.name}. Here's what I like about them: ${item.whatILike}`);
+          console.log(`I have ${chalk.magenta(item.count)} ${chalk.cyan(item.name)}. Here's what I like about them: ${chalk.green(item.whatILike)}`);
       }
 }
 //}
